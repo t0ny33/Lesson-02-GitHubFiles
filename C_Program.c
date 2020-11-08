0b05ace4917812f83ab7b769b9b3d14037425d8e
Ubuntu <enerazvan33@gmail.com>
2020-11-08 16:48:09 +0000

    I am pushing alll the files to my github as part of LEssion-02 laboratory

---
+++ C_Program.c
@@ -0,0 +1,5 @@
+include<stdio.h>
+int main(){
+printf("Hello! Iam a C program!")
+return 0;
+}
---
+++ HelloJava.java
@@ -0,0 +1,5 @@
+class HelloJava{
+	public static void main(String args[]){
+		System.out.Println("I am a Java Program. Thank yopu!");
+	}
+}
